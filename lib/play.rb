412884c7155abda8f93c1652e538f0af0ffd8147
Arielle D <domingu4@gmail.com>
2018-07-17 23:08:43 +0000

    automatically backed up by learn

--- lib/play.rb
+++ lib/play.rb
@@ -38,9 +38,9 @@ end
 # Define your play method below
 
 def play(board)
-  play(board) = 0
-  until play(board) == 8
-    turn(board)
-    play(board) += 1
+  play[board] = 0
+  until play[board] == 8
+    turn[board]
+    play[board] += 1
   end
 end
